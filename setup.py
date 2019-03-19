3d98882428c62b728e9e8d8ca3bc1e9bb2c40f96
Logan Ward <ward.logan.t@gmail.com>
2019-03-19 11:37:21 -0500

    Fixed bug in setup.py
    Renaming in PyCharm is a bit aggressive...

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 
 # single source of truth for package version
 version_ns = {}
-with open(os.path.join("materials_io.rst", "version.py")) as f:
+with open(os.path.join("materials_io", "version.py")) as f:
     exec(f.read(), version_ns)
 version = version_ns['__version__']
 
