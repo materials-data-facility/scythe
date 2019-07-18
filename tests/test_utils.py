ac9a4f951952c71bd13c399ebedace4f9f6740fb
Logan Ward <ward.logan.t@gmail.com>
2019-07-18 17:55:54 -0500

    Added a "run all parsers" function

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,6 +1,9 @@
-from materials_io.utils import get_available_parsers, execute_parser
+from materials_io.utils import get_available_parsers, execute_parser, run_all_parsers
 from materials_io.image import ImageParser
 import os
+
+
+cwd = os.path.dirname(__file__)
 
 
 def test_list_parsers():
@@ -8,5 +11,15 @@ def test_list_parsers():
 
 
 def test_execute_parser():
-    image = os.path.join(os.path.dirname(__file__), 'data', 'image', 'dog2.jpeg')
+    image = os.path.join(cwd, 'data', 'image', 'dog2.jpeg')
     assert ImageParser().parse([image]) == execute_parser('image', [image])
+
+
+def test_run_all_parsers():
+    path = os.path.join(cwd, 'data', 'image')
+    output = list(run_all_parsers(path))
+    assert len(output) > 0
+    assert len(output[0]) == 3
+    assert isinstance(output[0][0], tuple)
+    assert isinstance(output[0][1], str)
+    assert isinstance(output[0][2], dict)
