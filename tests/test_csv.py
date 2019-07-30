60418b0fab443e5449cdb717f350bfb096088423
Logan Ward <ward.logan.t@gmail.com>
2019-07-30 18:24:19 -0500

    Added ability to set context in `run_all_parsers`

--- tests/test_csv.py
+++ tests/test_csv.py
@@ -1,4 +1,5 @@
 from materials_io.csv import CSVParser
+from tableschema.types.geopoint import _geopoint
 import os
 
 csv_file = os.path.join(os.path.dirname(__file__), 'data', 'test.csv')
@@ -9,8 +10,11 @@ def test_csv():
 
     # Test with records
     output = p.parse([csv_file])
-    assert len(output['records']) == 3
+    assert len(output['records']) == 4
     assert isinstance(output['records'][0], dict)
+    assert isinstance(output['records'][0]['location'], list)
+    assert isinstance(output['records'][0]['location'][0], float)
+    assert isinstance(output['records'][-1]['location'], str)  # Would fail schema
     assert output['schema'] == {'fields': [{'name': 'city', 'type': 'string',
                                             'format': 'default'},
                                            {'name': 'location', 'type': 'geopoint',
@@ -20,6 +24,16 @@ def test_csv():
     # Test without records
     p.return_records = False
     assert 'records' not in p.parse([csv_file])
+
+    # Test with missing values
+    p.return_records = True
+    output = p.parse([csv_file], {'na_values': ['N/A']})
+    assert output['schema'] == {'fields': [{'name': 'city', 'type': 'string',
+                                            'format': 'default'},
+                                           {'name': 'location', 'type': 'geopoint',
+                                            'format': 'default'}],
+                                'missingValues': ['', 'N/A']}
+    assert output['records'][-1]['location'] is None
 
     # Just run the other operations
     assert any('https://github.com/frictionlessdata/tableschema-py' in x for x in p.citations())
