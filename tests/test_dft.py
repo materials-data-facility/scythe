a948749a07b8a12fe9d1bfd5b782e11cfa2afcbf
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-16 14:27:44 +0000

    Adding tarfile member sanitization to extractall()

--- tests/test_dft.py
+++ tests/test_dft.py
@@ -15,7 +15,26 @@ pwscf_tar = os.path.join(os.path.dirname(__file__), 'data',
 def vasp_dir(tmpdir):
     """Unpack VASP tar into a temporary directory"""
     with tarfile.open(vasp_tar) as fp:
-        fp.extractall(tmpdir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(fp, tmpdir)
     return str(tmpdir)
 
 
@@ -36,7 +55,26 @@ def multi_vasp_dir(vasp_dir):
 @pytest.fixture
 def pwscf_dir(tmpdir):
     with tarfile.open(pwscf_tar) as fp:
-        fp.extractall(tmpdir)
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(fp, tmpdir)
     return str(tmpdir)
 
 
