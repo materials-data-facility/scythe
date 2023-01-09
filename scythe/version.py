655c3b9a506efd794a96011b3e6b8ebfea92c303
Logan Ward <WardLT@users.noreply.github.com>
2023-01-09 16:40:34 -0500

    Prepare for a release to PyPI (#56)
    * Add the descriptions and such to the project page
    
    * Add a release flow
    
    Standard process, I used the one from Colmena
    
    * Increment version number
    
    So that we can test an autodeploy
    
    * Update the package name
    
    * Update README to reflect pypi publication
    
    * Update installation instructions in the docs

--- scythe/version.py
+++ scythe/version.py
@@ -4,4 +4,4 @@ import importlib.metadata
 # single source of truth for package version,
 # see https://packaging.python.org/en/latest/single_source_version/
 
-__version__ = importlib.metadata.version('scythe')
+__version__ = importlib.metadata.version('scythe-extractors')
